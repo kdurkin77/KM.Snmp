c1d3034475c742052b4baf8de699e9b77eaab27e
Krissy Durkin <kdurkin@ctdi.com>
2021-05-24 10:19:41 -0400

    Added cancellation handling for netstandard2.0's ReceiveAsync and SendAsync

--- Snmp/Extensions/SocketExtensions.cs
+++ Snmp/Extensions/SocketExtensions.cs
@@ -6,17 +6,39 @@ namespace System.Net.Sockets
     internal static class SocketExtensions
     {
 #if NETSTANDARD2_0
-        public static Task<int> SendAsync(this Socket socket, byte[] buffer, SocketFlags socketFlags, CancellationToken cancellationToken) =>
-            Task.Factory.FromAsync(
+        public static async Task<int> SendAsync(this Socket socket, byte[] buffer, SocketFlags socketFlags, CancellationToken cancellationToken)
+        {
+            var t = Task.Factory.FromAsync(
                 socket.BeginSend(buffer, 0, buffer.Length, socketFlags, null, socket),
                 socket.EndReceive
                 );
 
-        public static Task<int> ReceiveAsync(this Socket socket, byte[] buffer, SocketFlags socketFlags, CancellationToken cancellationToken) =>
-            Task.Factory.FromAsync(
+            if (t == await Task.WhenAny(t, Task.Delay(Timeout.Infinite, cancellationToken)).ConfigureAwait(false))
+            {
+                return await t.ConfigureAwait(false);
+            }
+            else
+            {
+                throw new TaskCanceledException();
+            }
+        }
+
+        public static async Task<int> ReceiveAsync(this Socket socket, byte[] buffer, SocketFlags socketFlags, CancellationToken cancellationToken)
+        {
+            var t = Task.Factory.FromAsync(
                 socket.BeginReceive(buffer, 0, buffer.Length, socketFlags, null, socket),
                 socket.EndReceive
                 );
+
+            if (t == await Task.WhenAny(t, Task.Delay(Timeout.Infinite, cancellationToken)).ConfigureAwait(false))
+            {
+                return await t.ConfigureAwait(false);
+            }
+            else
+            {
+                throw new TaskCanceledException();
+            }
+        }
 #endif
         public static async Task ConnectAsync(this Socket socket, IPEndPoint endpoint, CancellationToken cancellationToken)
         {
